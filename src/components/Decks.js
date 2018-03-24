67873165e70b9e3be083b4fae58849fbcbc41b17
YangKui <c6swift@gmail.com>
2018-03-24 17:06:17 +0800

    refactor: 抽出 deck 组件

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { FlatList, Text, TouchableOpacity } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native';
+import { get } from 'lodash/fp';
 import { Deck, Tips, CenterView } from './general';
 
+const DeckContainer = ({ item, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Deck title={item.title} count={item.questions.length} borderSize={1} />
+  </TouchableOpacity>
+);
 const Decks = ({ screenProps, navigation }) => {
-  const { decks } = screenProps;
+  const { decks, addCard } = screenProps;
 
   return decks.length ? (
     <FlatList
       data={decks}
-      renderItem={({ item }) => (
-        <TouchableOpacity
+      keyExtractor={get('title')}
+      renderItem={({ item, index }) => (
+        <DeckContainer
+          item={item}
           onPress={() =>
             navigation.navigate({
               routeName: 'DeckDetail',
-              params: { title: item.title, deck: item },
+              params: {
+                title: item.title,
+                deck: item,
+                addCard: addCard(index),
+              },
             })
           }
-          key={item.title}
-        >
-          <Deck
-            title={item.title}
-            count={item.questions.length}
-            borderSize={1}
-          />
-        </TouchableOpacity>
+        />
       )}
     />
   ) : (
